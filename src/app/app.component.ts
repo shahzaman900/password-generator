d4ca72be6c5459f7b8043f081a351622179f4126
Shah Zaman <shahzamanlive2@gmail.com>
2022-04-25 11:34:52 +0500

    UI complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pwg';
+    password = "";
+
+    onButtonClick() {
+        this.password = "My Password!!!!!!";
+    }
 }
