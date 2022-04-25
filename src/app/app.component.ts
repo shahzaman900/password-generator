b3c014ee5b8fa4356b33b145fda86c9b9bf07e03
Shah Zaman <shahzamanlive2@gmail.com>
2022-04-25 13:05:36 +0500

    pwg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,56 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    password = "";
+    length: number = 0;
+    includeLetters = false;
+    includeNumbers = false;
+    includeSymbols = false;
+    password = '';
 
     onButtonClick() {
-        this.password = "My Password!!!!!!";
+        const numbers = '1234567890';
+        const letters = 'abcdefghijklmnopqrstuvxyz';
+        const symbols = '!@#$%^&*()';
+
+        let validCharacters = '';
+        if (this.includeLetters) {
+            validCharacters += letters;
+        }
+
+        if (this.includeNumbers) {
+            validCharacters += numbers;
+        }
+
+        if (this.includeSymbols) {
+            validCharacters += symbols;
+        }
+
+        let generatePassword = '';
+        for (let i = 0; i < this.length; i++) {
+            const index = Math.floor(Math.random() * validCharacters.length);
+            generatePassword += validCharacters[index];
+        }
+
+        this.password = generatePassword;
+    }
+
+    onChangeLength(value: any) {
+        const newLength = parseInt(value.target.value);
+
+        if (!isNaN(newLength)) {
+            this.length = newLength;
+        }
+    }
+
+    onChangeUseLetter() {
+        this.includeLetters = !this.includeLetters;
+    }
+
+    onChangeUseNumber() {
+        this.includeNumbers = !this.includeNumbers;
+    }
+
+    onChangeUseSymbol() {
+        this.includeSymbols = !this.includeSymbols;
     }
 }
